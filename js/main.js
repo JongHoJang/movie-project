77d65d68325c32b03db6bf81d9da12341db2fe73
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2024-07-26 22:32:43 +0900

    1차 완성

--- js/main.js
+++ js/main.js
@@ -29,3 +29,17 @@ function ratingColor(rate, card) {
     $rating.classList.add("high");
   }
 }
+
+document.getElementById("search-input").addEventListener("input", () => {
+  const query = document.getElementById("search-input").value.toLowerCase();
+  const movieCards = document.querySelectorAll(".movie-card");
+
+  movieCards.forEach((card) => {
+    const title = card.querySelector("h3").textContent.toLowerCase();
+    if (title.includes(query)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
