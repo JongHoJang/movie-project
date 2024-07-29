820e2d9f7459bf330a04dcaae4adda2dd2ef92a8
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2024-07-29 22:00:12 +0900

    스크롤 인디케이터 추가

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+// 스크롤 인디케이터
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  const winScroll =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  const height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
 function createMovieCard(movie) {
   const card = document.createElement("div");
   card.className = "movie-card";
