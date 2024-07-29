7e17567fa176deb8ed75130bc2ace22b24aab4d2
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2024-07-29 15:55:22 +0900

    페이지 추가 및 모달

--- js/api.js
+++ js/api.js
@@ -1,14 +1,93 @@
 // 본인의 API 키를 넣어주셔야 합니다.
 const API_KEY = "59194452085a74c1cb8aae1c975a492a";
-const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en&page=1`;
+const baseUrl = "https://api.themoviedb.org/3/movie/";
+const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // 요청할 페이지 번호
 
-fetch(URL)
-  .then((response) => response.json())
-  .then((data) => {
-    const movies = data.results;
+//
+//
+//Popular Movies
+// 단일 페이지의 데이터를 요청하고 처리
+async function fetchMovies(page) {
+  const url = `${baseUrl}popular?api_key=${API_KEY}&language=en&page=${page}`;
+  const response = await fetch(url); // url로 fetch 요청(get)
+  const data = await response.json(); // json형태로 데이터 가져옴
+  return data.results; // 영화 데이터만 반환
+}
+
+// 단일 페이지의 데이터를 map 함수를 통해 pages를 돌며 가져옴
+async function fetchAllMovies(pages) {
+  const promises = pages.map((page) => fetchMovies(page));
+  const results = await Promise.all(promises);
+  console.log(results);
+  console.log(results.flat());
+  return results.flat(); // 2차원 배열을 1차원으로 변환
+}
+
+// 페이지 데이터를 요청하고 처리하는 함수
+fetchAllMovies(pages)
+  .then((movies) => {
     const movieContainer = document.getElementById("movie-container");
     movies.forEach((movie) => {
       const card = createMovieCard(movie);
+      movieContainer.appendChild(card);
+    });
+  })
+  .catch((error) => console.error("Error:", error));
+
+//
+//
+//Rated Movie Card
+async function fetchRatedMovies(page) {
+  const url = `${baseUrl}top_rated?api_key=${API_KEY}&language=en&page=${page}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.results;
+}
+
+async function fetchRatedAllMovies(pages) {
+  const promises = pages.map((page) => fetchRatedMovies(page));
+  const results = await Promise.all(promises);
+  console.log(results);
+  console.log(results.flat());
+  return results.flat();
+}
+
+fetchRatedAllMovies(pages)
+  .then((movies) => {
+    const movieContainer = document.getElementById("rated-movie-container");
+    movies.forEach((movie) => {
+      const card = createRatedMovieCard(movie);
+      movieContainer.appendChild(card);
+    });
+  })
+  .catch((error) => console.error("Error:", error));
+
+//
+//
+// Now Playing Movie Card
+async function fetchNowPlayingMovies(page) {
+  const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=ko&region=kr&page=${page}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.results;
+}
+
+async function fetchNowPlayingAllMovies(pages) {
+  const promises = pages.map((page) => fetchNowPlayingMovies(page));
+  const results = await Promise.all(promises);
+  return results.flat();
+}
+
+fetchNowPlayingAllMovies(pages)
+  .then((movies) => {
+    const resultCountElement = document.querySelector(".result-count");
+    resultCountElement.textContent = `현재 한국에서 상영 중인 영화: ${movies.length}편`;
+
+    const movieContainer = document.getElementById(
+      "now-playing-movie-container"
+    );
+    movies.forEach((movie) => {
+      const card = createNowPlayingMovieCard(movie);
       movieContainer.appendChild(card);
     });
   })
--- js/main.js
+++ js/main.js
@@ -9,19 +9,55 @@ function createMovieCard(movie) {
     <p>${movie.overview}</p>
     <span class="rating">Rating: ${movie.vote_average.toFixed(2)}</span>
   `;
-  card.addEventListener("click", () => alert(`Movie ID: ${movie.id}`));
 
+  // 모달창
+  card.addEventListener("click", () => {
+    const modal = document.getElementById("modalContainer");
+    const modalMovieId = document.querySelector(".modalMovieId");
+    modalMovieId.innerHTML = `
+    <div>
+     <img class="modalImg" src="https://image.tmdb.org/t/p/w500${
+       movie.poster_path
+     }" alt="${movie.title}">
+    </div>
+        <div>
+          <h3>${movie.title}</h3>
+          <p>${movie.overview}</p>
+          <p>Rating: ${movie.vote_average.toFixed(2)}</p>
+          <p>Movie ID: ${movie.id}</p>
+       </div>`;
+
+    modal.classList.remove("hidden");
+  });
   ratingColor(movie.vote_average, card);
 
   return card;
 }
+
+// 모달 관련 요소 선택
+const modalCloseBtn = document.getElementById("modalCloseBtn");
+const modal = document.getElementById("modalContainer");
+
+// 모달 닫기 버튼 클릭 시 모달 창 숨기기
+modalCloseBtn.addEventListener("click", () => {
+  modal.classList.add("hidden");
+});
+
+// 배경 클릭 시 모달 창 숨기기
+window.addEventListener("click", (event) => {
+  if (event.target == modal) {
+    modal.classList.add("hidden");
+  }
+});
 
 function ratingColor(rate, card) {
   const $rating = card.querySelector("span");
   $rating.className = "rating"; // 초기화
 
   // 평점에 따른 클래스 추가
-  if (rate < 6) {
+  if (rate === 0) {
+    $rating.classList.add("zero");
+  } else if (rate < 6) {
     $rating.classList.add("low");
   } else if (rate >= 6 && rate < 8) {
     $rating.classList.add("medium");
@@ -30,6 +66,7 @@ function ratingColor(rate, card) {
   }
 }
 
+//검색창
 document.getElementById("search-input").addEventListener("input", () => {
   const query = document.getElementById("search-input").value.toLowerCase();
   const movieCards = document.querySelectorAll(".movie-card");
@@ -43,3 +80,73 @@ document.getElementById("search-input").addEventListener("input", () => {
     }
   });
 });
+
+//RatedMovie
+function createRatedMovieCard(movie) {
+  const card = document.createElement("div");
+  card.className = "movie-card";
+  card.innerHTML = `
+    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${
+    movie.title
+  }">
+    <h3>${movie.title}</h3>
+    <p>${movie.overview}</p>
+    <span class="rating">Rating: ${movie.vote_average.toFixed(2)}</span>
+  `;
+
+  // 모달창
+  card.addEventListener("click", () => {
+    const modal = document.getElementById("modalContainer");
+    const modalMovieId = document.querySelector(".modalMovieId");
+    modalMovieId.innerHTML = `
+        <div>
+          <h3>${movie.title}</h3>
+          <p>${movie.overview}</p>
+          <p>Rating: ${movie.vote_average.toFixed(2)}</p>
+          <p>Movie ID: ${movie.id}</p>
+       </div>`;
+
+    modal.classList.remove("hidden");
+  });
+  ratingColor(movie.vote_average, card);
+
+  return card;
+}
+
+//
+//
+// Now Playing Movie Card
+function createNowPlayingMovieCard(movie) {
+  const card = document.createElement("div");
+  card.className = "movie-card";
+  card.innerHTML = `
+    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${
+    movie.title
+  }">
+    <h3>${movie.title}</h3>
+    <p>${movie.overview}</p>
+    <span class="rating">Rating: ${movie.vote_average.toFixed(2)}</span>
+  `;
+  card.addEventListener("click", () => {
+    const modal = document.getElementById("modalContainer");
+    const modalMovieId = document.querySelector(".modalMovieId");
+    modalMovieId.innerHTML = `
+    <div>
+     <img class="modalImg" src="https://image.tmdb.org/t/p/w500${
+       movie.poster_path
+     }" alt="${movie.title}">
+    </div>
+        <div>
+          <h3>${movie.title}</h3>
+          <p>${movie.overview}</p>
+          <p>Rating: ${movie.vote_average.toFixed(2)}</p>
+          <p>Movie ID: ${movie.id}</p>
+
+       </div>`;
+
+    modal.classList.remove("hidden");
+  });
+  ratingColor(movie.vote_average, card);
+
+  return card;
+}
